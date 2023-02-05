f8794f74ffe3557dba96ca79a1db3c9d0f43bd02
elizard-beth <71954340+elizard-beth@users.noreply.github.com>
2023-02-06 12:19:51 +1300

    Create Update.module.lua

---
+++ src/OS/Sword/Update.module.lua
@@ -0,0 +1,14 @@
+--[[
+OSes need a way to update to their latest release
+Some users prefer to use older versions, so giving them the option to receive 
+updates is needed.
+]]
+
+local System = {
+	SystemFile = true,
+	Required = true
+}
+
+local function Update()
+	
+end
